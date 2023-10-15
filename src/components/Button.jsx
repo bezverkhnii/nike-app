3ff3f6b3cf27731c02436bd7418c917126d1e6a2
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2023-10-15 12:34:09 +0200

    implemented popular products section and quality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,13 @@ const Button = ({ label, iconUrl }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red text-white border-coral-red rounded-full">
       {label}
-      <img
-        src={iconUrl}
-        alt="arrow right"
-        className="ml-2 rounded-full w-5 h-5"
-      ></img>
+      {iconUrl && (
+        <img
+          src={iconUrl}
+          alt="arrow right"
+          className="ml-2 rounded-full w-5 h-5"
+        ></img>
+      )}
     </button>
   );
 };
