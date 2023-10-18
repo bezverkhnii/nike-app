9f23ebdd3ea7cd92ae1f8335689947588468a6a7
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2023-10-18 11:48:24 +0200

    finished landing page implementation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,19 @@
-const Button = ({ label, iconUrl }) => {
+const Button = ({
+  label,
+  iconUrl,
+  backgroundColor,
+  borderColor,
+  textColor,
+  fullwidth,
+}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red text-white border-coral-red rounded-full">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none ${
+        backgroundColor ? backgroundColor : "bg-coral-red"
+      }  ${textColor ? textColor : "text-white"} ${
+        borderColor ? borderColor : "border-coral-red"
+      } rounded-full ${fullwidth && "w-full"}`}
+    >
       {label}
       {iconUrl && (
         <img
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,5 +1,19 @@
+import { services } from "../constants";
+import ServiceCard from "../components/ServiceCard";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard
+          key={service.label}
+          label={service.label}
+          subtext={service.subtext}
+          img={service.imgURL}
+        />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
